e09400ce212d4b11aa39e89d7022e7ae7997ccff
PriyaBarnwal <priya.barnwal@gmail.com>
2017-02-07 16:55:12 +0530

    urlgeneration

--- app/src/main/java/com/gmail/kgec/project/MainActivity.java
+++ app/src/main/java/com/gmail/kgec/project/MainActivity.java
@@ -1,10 +1,12 @@
 package com.gmail.kgec.project;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,26 +16,22 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-EditText ed;
+    EditText edt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
+        edt=(EditText)findViewById(R.id.edit1);
     }
-
-
-    public void btnC(View view) {
-
-        //Input to execute function is to be changed
-        new JsonTask().execute("https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesDemoItem.txt");
-
+    public void sc1(View v) {
+        String str = edt.getText().toString();
+        str.toLowerCase();
+        str = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+        StringBuffer sb=new StringBuffer("https://data.gov.in/api/datastore/resource.json?resource_id=37670b6f-c236-49a7-8cd7-cc2dc610e32d&api-key=fabf9d77e3e39763802f22b2bc927d25&filters[District]="+str+"&fields=Location_Coordinates%2CLocation%2CHospital_Name%2CHospital_Category%2CHospital_Care_Type%2CDiscipline_Systems_of_Medicine%2CAddress_Original_First_Line%2CState%2CDistrict%2CSubdistrict%2CPincode%2CTelephone%2CMobile_Number%2CEmergency_Num%2CAmbulance_Phone_No%2CBloodbank_Phone_No%2CHospital_Fax%2CHospital_Primary_Email_Id%2CWebsite%2CSpecialties%2CFacilities%2CAccreditation%2CHospital_Regis_Number%2CNumber_Doctor%2CTotal_Num_Beds%2CNumber_Private_Wards%2C&sort[Hospital_Name]=asc");
+        String url=sb.toString();
+        Toast.makeText(getApplicationContext(),"searching...",Toast.LENGTH_SHORT).show();
+        new JsonTask().execute(url);
     }
-
-
     class JsonTask extends AsyncTask<String,String,String> {
         HttpURLConnection con;
         BufferedReader br;
@@ -75,6 +73,9 @@ EditText ed;
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            Intent nextscreen=new Intent(getApplicationContext(),ResultView.class);
+            nextscreen.putExtra("passarg",s);
+            startActivity(nextscreen);
             //create Intent and send the String s to ResultView
         }
 
